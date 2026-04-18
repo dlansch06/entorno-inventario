6b5979529550a8b77c5edc1dda6d3d18afebf14c
dlansch06 <bettynoasanchez@gmail.com>
2026-04-17 19:28:15 -0500

    configuraciones adicionales

--- sistema_colegio/settings.py
+++ sistema_colegio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Al final del archivo settings.py
 LOGIN_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
