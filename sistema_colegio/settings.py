b41799de95d2b9d7fb666de9f1d7f2a64dbb7bb7
dlansch06 <bettynoasanchez@gmail.com>
2026-04-23 13:32:07 -0500

    Corregida ruta de templates y nombre de login.html para Render

--- sistema_colegio/settings.py
+++ sistema_colegio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sistema_colegio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'inventario' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
